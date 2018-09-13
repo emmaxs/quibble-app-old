bb6609305785f7215599e99dbc78328f5acbcf21
emmaxs <emmaxs@users.noreply.github.com>
2018-09-12 22:22:26 -0400

    Basic Picker Functionality
    Picker implemented in a hard-coding kind of way

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -26,106 +26,29 @@ export default class LibraryScreen extends React.Component {
     /* Hardcoded library picker */
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
+        <ScrollView style={styles.container}>
           /* temporary picker */
           <View>
-            {this._maybeRenderDevelopmentModeWarning()}
             <Picker selectedValue = {this.state.user} onValueChange = {this.updateUser}>
-               <Picker.Item label = "Steve" value = "steve" />
-               <Picker.Item label = "Ellen" value = "ellen" />
-               <Picker.Item label = "Maria" value = "maria" />
+               <Picker.Item label = "Two-Point-Six" value = "TPS" />
+               <Picker.Item label = "Dracula" value = "D" />
+               <Picker.Item label = "Frankenstein" value = "F" />
             </Picker>
-            <Text style={{fontSize: 20, alignSelf: 'center'}}> {this.state.user}</Text>
+            // This shows that the state was changed
+            <Text style={{fontSize: 20, alignSelf: 'center'}}> You chose: {this.state.user}</Text>
+            <Text style={{fontSize: 16, alignSelf: 'center', fontStyle: 'italic', padding: 10}}>
+            But actually only TPS is available
+            </Text>
           </View>
         </ScrollView>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
